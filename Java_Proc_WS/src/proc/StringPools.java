6e0e757bb0eb5b74cbbea84d020efecb53dc7985
Himanshu Bhandari <himanshubhandari546@gmail.com>
2024-05-25 23:25:22 +0530

    "String pools codebase commit"

---
+++ Java_Proc_WS/src/proc/StringPools.java
@@ -0,0 +1,23 @@
+package proc;
+
+public class StringPools {
+
+	public static void main(String[] args) {
+		String s1 = "java";
+		String s2 = "user";
+		String s3 = s1.concat(s2);
+		String s4 = new String("javauser");
+		String s5 = "java";
+		String s6 = new String("javauser");
+
+		System.out.println(s1 == s5);
+		System.out.println(s1.equals(s5));
+		System.out.println(s1.equals(s2));
+		System.out.println(s2.equals(s3));
+		System.out.println(s3.equals(s4));
+		System.out.println(s3.equals(s4.intern()));
+		System.out.println(s6.intern().equals(s4.intern()));
+		System.out.println(s6.equals(s4));
+	}
+
+}

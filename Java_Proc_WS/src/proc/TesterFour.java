5611c02c50dbca8a3bcc4f28d009a5c302c3089d
Himanshu Bhandari <himanshubhandari546@gmail.com>
2024-05-25 22:25:24 +0530

    "Right shift operators codebase"

---
+++ Java_Proc_WS/src/proc/TesterFour.java
@@ -0,0 +1,16 @@
+package proc;
+
+public class TesterFour {
+
+	public static void main(String[] args) {
+	     int a = -10, b = 3;
+	     // Use of >>
+	     System.out.println(a >> b);
+
+	     a = 3;
+	     b = 1;
+	     // Use of >>>
+	     System.out.println(a >>> b);
+	 }
+
+}

bac04020f4ee4247ef761074a7a54a539c862a82
Himanshu Bhandari <himanshubhandari546@gmail.com>
2024-05-25 22:44:17 +0530

    "Merge Armstrong numbers algo"

--- Java_Proc_WS/src/proc/ReverseOrSumOfANumber.java
+++ Java_Proc_WS/src/proc/ReverseOrSumOfANumber.java
@@ -3,9 +3,22 @@ package proc;
 public class ReverseOrSumOfANumber {
 
 	public static void main(String[] args) {
-		int number = 197;
+		int number = 153;
 		doReverseOfNumber(number);
 		doSumOfDigitsOfNumber(number);
+		isArmstrongNumber(number);
+	}
+
+	public static void isArmstrongNumber(int number) {
+		int sum = 0;
+		int originalNumber = number;
+		while (number != 0) {
+			int i = number;
+			number = number % 10;
+			sum = sum + ( number * number * number );
+			number = i / 10;
+		}
+		System.out.println("Is Armstrong Number?? : " + (sum == originalNumber) );
 	}
 
 	public static void doReverseOfNumber(int number) {

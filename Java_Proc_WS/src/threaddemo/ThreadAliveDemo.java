2d88461aead3ad342b5cfd71d2f9f9c51fcc3896
Himanshu Bhandari <himanshubhandari546@gmail.com>
2024-05-26 16:35:37 +0530

    "Thread alive demo"

---
+++ Java_Proc_WS/src/threaddemo/ThreadAliveDemo.java
@@ -0,0 +1,25 @@
+package threaddemo;
+
+public class ThreadAliveDemo extends Thread {
+
+	public static int amount = 0;
+
+	public static void main(String[] args) {
+		ThreadAliveDemo thread = new ThreadAliveDemo();
+		thread.start();
+		// Wait for the thread to finish
+		while (thread.isAlive()) {
+			System.out.println("Waiting...");
+		}
+		// Update amount and print its value
+		System.out.println("ThreadAliveDemo: " + amount);
+		amount++;
+		System.out.println("ThreadAliveDemo: " + amount);
+	}
+
+	public void run() {
+		amount++;
+		//System.out.println("ThreadAliveDemo inside run : " + amount);
+	}
+
+}

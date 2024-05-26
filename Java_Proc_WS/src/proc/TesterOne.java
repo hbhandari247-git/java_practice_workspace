304fc24327ebb96d1ba7ab4df01c1b565e307947
Himanshu Bhandari <himanshubhandari546@gmail.com>
2024-05-26 13:36:51 +0530

    "BITWISE CODE TESTER ADDITION"

--- Java_Proc_WS/src/proc/TesterOne.java
+++ Java_Proc_WS/src/proc/TesterOne.java
@@ -4,5 +4,7 @@ public class TesterOne {
 	public static void main(String[] args) {
 		int num = 343;
 		System.out.println(num / 10 - 2 * (num - num / 10 * 10));
+		
+		System.out.println(2657860 & (16-1)); //BITWISE AND OPERATOR
 	}
 }

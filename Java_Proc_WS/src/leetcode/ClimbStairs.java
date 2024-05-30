eae7e0bf4ef16c47db075321f45bc8472af9581d
Himanshu Bhandari <himanshubhandari546@gmail.com>
2024-05-30 13:43:57 +0530

    "Java docs"

--- Java_Proc_WS/src/leetcode/ClimbStairs.java
+++ Java_Proc_WS/src/leetcode/ClimbStairs.java
@@ -3,6 +3,7 @@ package leetcode;
 /**
  * You are climbing a staircase. It takes n steps to reach the top.
  * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ * f(n)=f(n−1)+f(n−2)
  */
 public class ClimbStairs {
 	public static void main(String[] args) {

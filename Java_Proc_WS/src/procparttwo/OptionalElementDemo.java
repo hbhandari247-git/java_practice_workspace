6b44415520b879d42827bb1cfb12187672251711
Himanshu Bhandari <himanshubhandari546@gmail.com>
2024-06-01 09:29:26 +0530

    "Optional Example demo - 2"

--- Java_Proc_WS/src/procparttwo/OptionalElementDemo.java
+++ Java_Proc_WS/src/procparttwo/OptionalElementDemo.java
@@ -1,12 +1,13 @@
 package procparttwo;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 public class OptionalElementDemo {
 
 	public static void main(String[] args) {
 		String[] stringArray = new String[5];
-		stringArray[2] = "Hello World!";
+		stringArray[2] = "GeeksForGeeks";
 		Optional<String> empty = Optional.empty();
 		System.out.println(empty);
 
@@ -15,6 +16,19 @@ public class OptionalElementDemo {
 
 		Optional<String> value2 = Optional.ofNullable(stringArray[0]);
 		System.out.println(value2);
+
+		boolean isPresentValue = Optional.ofNullable(stringArray[0]).isPresent();
+		System.out.println(isPresentValue);
+
+		Optional.ofNullable(stringArray[2]).ifPresent(element -> {
+			for (int i = 0; i < element.length(); i++) {
+				char ch = element.charAt(i);
+
+				if (element.indexOf(ch) == element.lastIndexOf(element)) {
+					System.out.println("" + ch);
+				}
+			}
+		});;
 	}
 
 }

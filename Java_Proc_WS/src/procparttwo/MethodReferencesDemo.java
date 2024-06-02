da952476e8e2f63d264105f740c1e28204d306be
Himanshu Bhandari <himanshubhandari546@gmail.com>
2024-06-02 08:42:00 +0530

    "method references demo"

--- Java_Proc_WS/src/procparttwo/MethodReferencesDemo.java
+++ Java_Proc_WS/src/procparttwo/MethodReferencesDemo.java
@@ -2,12 +2,19 @@ package procparttwo;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 public class MethodReferencesDemo {
 
 	public static void main(String[] args) {
 		List<String> messages = Arrays.asList("hello", "baeldung", "readers!");
 		messages.stream().map(element -> element).forEach(System.out::println);
-		System.out.println(messages);
+		
+		// Instance method reference
+		Function<String, String> toLowerCase = String::toLowerCase;
+		// Static method reference
+		Function<String, Integer> strLength = String::length;
+		System.out.println(toLowerCase.apply("ABC"));
+		System.out.println(strLength.apply("ABC"));
 	}
 }

9c75b75ef1e3eaea72d74eeec1f7265af0b93500
Himanshu Bhandari <himanshubhandari546@gmail.com>
2024-06-03 11:44:18 +0530

    "Minor changes"

--- Java_Proc_WS/src/procparttwo/CheckIfNumberIsPrimeDemo.java
+++ Java_Proc_WS/src/procparttwo/CheckIfNumberIsPrimeDemo.java
@@ -2,8 +2,12 @@ package procparttwo;
 
 public class CheckIfNumberIsPrimeDemo {
 	public static void main(String[] args) {
-		int number = 38;
+		int number = 2;
 		boolean flag = false;
+
+		if (number <= 1) {
+			flag = true;
+		}
 
 		for (int i = 2; i <= number / 2; i++) {
 			if (number % i == 0) {

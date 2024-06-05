6af3ae811b4872787a1391084ece3bebf7acf539
Himanshu Bhandari <himanshubhandari546@gmail.com>
2024-06-05 12:34:24 +0530

    "Kadane Algorithm"

--- Java_Proc_WS/src/procparttwo/KadaneAlgorithmDemo.java
+++ Java_Proc_WS/src/procparttwo/KadaneAlgorithmDemo.java
@@ -1,20 +1,24 @@
 package procparttwo;
 
 public class KadaneAlgorithmDemo {
+	// Driver Code
 	public static void main(String[] args) {
-		int[] intArray = { -2, 3, 4, -1, 2, 1, -5, 3 };
+		int[] a = { -2, -3, 4, -1, -2, 1, 5, -3 };
+		System.out.println("Maximum contiguous sum is " + maxSubArraySum(a));
+	}
 
-		int answer = Integer.MIN_VALUE;
+	// Function Call
+	public static int maxSubArraySum(int a[]) {
+		int size = a.length;
+		int max_so_far = Integer.MIN_VALUE, max_ending_here = 0;
 
-		int curr_sum = 0;
-
-		for (int i = 0; i < intArray.length; i++) {
-			curr_sum += intArray[i];
-			answer = Math.max(answer, curr_sum);
-			if (curr_sum < 0) {
-				curr_sum = 0;
-			}
+		for (int i = 0; i < size; i++) {
+			max_ending_here = max_ending_here + a[i];
+			if (max_so_far < max_ending_here)
+				max_so_far = max_ending_here;
+			if (max_ending_here < 0)
+				max_ending_here = 0;
 		}
-		System.out.println(answer);
+		return max_so_far;
 	}
 }

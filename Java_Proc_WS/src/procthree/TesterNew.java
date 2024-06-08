f28da396a1beb64b8e487dc183daf5701f55a31f
Himanshu Bhandari <himanshubhandari546@gmail.com>
2024-06-08 20:15:47 +0530

    "IdentityHashMap"

--- Java_Proc_WS/src/procthree/TesterNew.java
+++ Java_Proc_WS/src/procthree/TesterNew.java
@@ -1,14 +1,33 @@
 package procthree;
 
-import java.util.*;
+import java.util.IdentityHashMap;
+import java.util.Map;
 
 public class TesterNew {
 	public static void main(String[] args) {
-		String s1 = "abccddaaeeffg";
-		HashMap<String, Integer> stringHashMap = new HashMap<String, Integer>();
-		for (int i = 0; i < s1.toCharArray().length; i++) {
-			stringHashMap.put("" + s1.charAt(i), stringHashMap.getOrDefault("" + s1.charAt(i), 0) + 1);
-		}
-		System.out.println(stringHashMap);
+		Integer i1 = 1000;
+		Integer i2 = 1000;
+		int i3 = 100;
+		System.out.println(i1 == i2);
+		System.out.println(i1 == i3);
+		System.out.println(i2 == i3);
+		System.out.println(Integer.MAX_VALUE);
+
+		Map<String, String> map = new IdentityHashMap<>();
+
+		// Creating two different instances of String with the same value
+		String key1 = new String("key");
+		String key2 = new String("key");
+
+		// Adding the key-value pairs to the IdentityHashMap
+		map.put(key1, "value1");
+		map.put(key2, "value2");
+
+		// Checking the size of the map
+		System.out.println("Size of map: " + map.size()); // Output: 2
+
+		// Checking the values associated with the keys
+		System.out.println("Value for key1: " + map.get(key1)); // Output: value1
+		System.out.println("Value for key2: " + map.get(key2)); // Output: value2
 	}
 }

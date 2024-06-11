1bd8ae804fe53ed5bee7b9aa64863341a5fbe663
Himanshu Bhandari <himanshubhandari546@gmail.com>
2024-06-11 20:45:25 +0530

    "Rearrange Array Code Commit"

---
+++ Java_Proc_WS/src/procthree/RearrangeArray.java
@@ -0,0 +1,31 @@
+package procthree;
+
+import java.util.Arrays;
+
+public class RearrangeArray {
+	public static void rearrange(int[] arr) {
+		int left = 0, right = arr.length - 1;
+
+		while (left <= right) {
+			if (arr[left] < 0 && arr[right] >= 0) {
+				int temp = arr[left];
+				arr[left] = arr[right];
+				arr[right] = temp;
+				left++;
+				right--;
+			}
+			if (arr[left] >= 0) {
+				left++;
+			}
+			if (arr[right] < 0) {
+				right--;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 12, -7, 5, -3, 9, -2, -8, 4, 10 };
+		rearrange(arr);
+		System.out.println("Rearranged array: " + Arrays.toString(arr));
+	}
+}
